8cd02a90aa1b011f58b26b1f6c813042442b330c
aliasbsv <aliasbsv@gmail.com>
2024-11-24 15:42:12 +0100

    fix error

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,2 @@
-
-import path from 'node:path';
-
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAY = 30 * 24 * 60 * 60 * 1000;
-export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
-
