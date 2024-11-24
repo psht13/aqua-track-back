4fcaf9f9206f5e5b8ca8198589819ae85666641b
aliasbsv <aliasbsv@gmail.com>
2024-11-24 01:40:24 +0100

    Creating endpoint documentation

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
+import path from 'node:path';
+
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAY = 30 * 24 * 60 * 60 * 1000;
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
