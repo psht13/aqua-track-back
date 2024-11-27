f7994931bd99be06446bd2ea51dd02fac9cefc99
Olha Nahaichuk <olyasikaa@gmail.com>
2024-11-27 13:50:24 +0200

    changes + documentation /refresh + /logout

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,7 +3,6 @@ import createHttpErrors from 'http-errors';
 import { updateUser } from '../services/users.js';
 import { uploadToCloudinary } from '../utils/upload-to-cloudinary.js';
 import { saveFileToUploadDir } from '../utils/save-file-to-upload-dir.js';
-import { refreshSession, logoutUser } from '../services/users.js';
 import { ENABLE_CLOUDINARY } from '../constants/index.js';
 
 export const patchUserController = async (req, res, next) => {
@@ -34,38 +33,7 @@ export const patchUserController = async (req, res, next) => {
     data: result.user,
   });
 };
-/* Controller for user logout.  */
-export async function logoutController(req, res) {
-  const { sessionId } = req.cookies;
-  if (typeof sessionId === 'string') {
-    await logoutUser(sessionId);
-  }
-  res.clearCookie('refreshToken');
-  res.clearCookie('sessionId');
-  res.status(204).end();
-}
-/* Refresh Controller . Updates the user's refresh token by validating the existing refresh token
- * and issuing a new pair of access and refresh tokens. */
 
-export async function refreshController(req, res) {
-  const { sessionId, refreshToken } = req.cookies;
-  const session = await refreshSession(sessionId, refreshToken);
-  res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: session.refreshTokenValidUntil,
-  });
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: session.refreshTokenValidUntil,
-  });
-  res.send({
-    status: 200,
-    message: 'Successfully refreshed a session!',
-    data: {
-      accessToken: session.accessToken,
-    },
-  });
-}
 export const currentUserController = async (req, res) => {
   const { user } = req;
   if (!user) {
