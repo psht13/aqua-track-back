15d34b13bcb91118ca10380359eb2b180514aede
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-12-06 08:51:39 +0200

    update: validation

--- src/validation/users.js
+++ src/validation/users.js
@@ -6,9 +6,22 @@ export const updateUserSchema = Joi.object({
     'string.min': 'User should have at least 3 characters ',
     'string.max': 'User should have at most 20 characters',
   }),
-  email: Joi.string().min(3).max(20),
-  gender: Joi.string().valid('woman', 'man'),
-  weight: Joi.number(),
-  activeTime: Joi.number(),
-  dailyNorm: Joi.number(),
+  email: Joi.string().min(3).max(50).messages({
+    'string.base': 'Email must be a string',
+    'string.min': 'Email must be at least 3 characters long',
+    'string.max': 'Email must be at most 50 characters long',
+  }),
+  gender: Joi.string().valid('woman', 'man').messages({
+    'string.base': 'Gender must be a string',
+    'string.valid': 'Gender must be "woman" or "man"',
+  }),
+  weight: Joi.number().messages({
+    'number.base': 'Weight must be a number',
+  }),
+  activeTime: Joi.number().messages({
+    'number.base': 'Weight must be a number',
+  }),
+  dailyNorm: Joi.number().messages({
+    'number.base': 'Weight must be a number',
+  }),
 });
